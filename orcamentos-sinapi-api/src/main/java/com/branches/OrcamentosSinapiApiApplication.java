2028fcca243047d54c28e7c46b2b0d3963eff35d
mbranches <marcusvbranches@gmail.com>
2025-03-07 19:55:21 -0300

    feat: adicionando package api springboot

---
+++ orcamentos-sinapi-api/src/main/java/com/branches/OrcamentosSinapiApiApplication.java
@@ -0,0 +1,13 @@
+package com.branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentosSinapiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentosSinapiApiApplication.class, args);
+	}
+
+}
