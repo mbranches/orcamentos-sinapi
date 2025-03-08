1003bd479e50de59382ff081e66143ca3973f032
Branches <marcus.branches@icloud.com>
2025-03-08 14:23:46 -0300

    feat: model Servico adicionado

---
+++ orcamentos-sinapi-api/src/main/java/com/branches/model/Servico.java
@@ -0,0 +1,18 @@
+package com.branches.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Servico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String codigo;
+    @Column(name = "descricao_composicao")
+    private String descricao;
+    @Column(name = "unidade_medida")
+    private String unidadeMedida;
+    @Column(name = "origem_preco")
+    private String origemPreco;
+    private Double preco;
+}
