74f5692c0085d9dfac05322c1d95e1a3273e9495
mbranches <marcusvbranches@gmail.com>
2025-03-09 16:37:46 -0300

    refactor: nome da entidade alterado

--- orcamentos-sinapi-api/src/main/java/com/branches/model/Orcamento.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/model/Orcamento.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
-@Entity
+@Entity(name = "orcamento")
 public class Orcamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
