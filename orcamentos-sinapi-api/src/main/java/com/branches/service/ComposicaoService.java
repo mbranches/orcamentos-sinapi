42fb1489b2f1b8c9b08566e8ed2ff5a466c7b31d
mbranches <marcusvbranches@gmail.com>
2025-03-09 16:56:08 -0300

    feat: ComposicaoRepository e ComposicaoController adicionados

---
+++ orcamentos-sinapi-api/src/main/java/com/branches/service/ComposicaoService.java
@@ -0,0 +1,22 @@
+package com.branches.service;
+
+import com.branches.model.Composicao;
+import com.branches.repository.ComposicaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ComposicaoService {
+    private final ComposicaoRepository repository;
+
+    public List<Composicao> findAll() {
+        return repository.findAll();
+    }
+
+    public List<Composicao> findByDescription(String description) {
+        return repository.findAllByDescricaoContaining(description);
+    }
+}
