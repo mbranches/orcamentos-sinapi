f239fd66865e95fff6233d93c1d40590a1b20b6e
mbranches <marcusvbranches@gmail.com>
2025-03-10 15:34:52 -0300

    feat: tornando request param não obrigatorio

--- orcamentos-sinapi-api/src/main/java/com/branches/controller/InsumoController.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/controller/InsumoController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("v1/insumos")
 @RequiredArgsConstructor
-@RestController("v1/insumos")
+@RestController
 public class InsumoController {
     private final InsumoService service;
 
     @GetMapping
-    public List<Insumo> findAll(@RequestParam String description) {
+    public List<Insumo> findAll(@RequestParam(required = false) String description) {
         return description == null ? service.findAll() : service.findByDescription(description);
     }
 
