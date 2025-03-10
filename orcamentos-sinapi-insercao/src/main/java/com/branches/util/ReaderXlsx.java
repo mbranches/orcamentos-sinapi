ec9f79ffdec9824fca9c76fe8c2b7c933d6f0f47
mbranches <marcusvbranches@gmail.com>
2025-03-10 15:36:30 -0300

    feat: método para ler a planilha sinapi adicionado

--- orcamentos-sinapi-insercao/src/main/java/com/branches/util/ReaderXlsx.java
+++ orcamentos-sinapi-insercao/src/main/java/com/branches/util/ReaderXlsx.java
@@ -1,5 +1,81 @@
 package com.branches.util;
 
-public class ReaderXlsx {
+import com.branches.request.InsumoPostRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ReaderXlsx {
+    private final ResourceLoader resourceLoader;
+
+    public List<InsumoPostRequest> readerAllInsumos() throws IOException, InvalidFormatException {
+        List<InsumoPostRequest> insumos = new ArrayList<>();
+
+        String path = "files/SINAPI_Preco_Ref_Insumos_PA_202412_NaoDesonerado.xlsx";
+        File file = resourceLoader.getResource("classpath:%s".formatted(path)).getFile();
+        Workbook workbook = new XSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (Row row : sheet) {
+            if (row.getRowNum() <= 7 || row.getRowNum() >= 4846) continue;
+            InsumoPostRequest insumo = InsumoPostRequest.builder().build();
+
+            for (Cell cell : row) {
+                System.out.println(cell.toString());
+                if (!cell.toString().isEmpty()) continue;
+
+                switch (cell.getColumnIndex()) {
+                    case 0:
+                        insumo.setCodigo(cell.toString());
+                        break;
+                    case 1:
+                        insumo.setDescricao(cell.toString().trim());
+                        break;
+                    case 2:
+                        insumo.setUnidadeMedida(cell.toString().trim());
+                        break;
+                    case 3:
+                        insumo.setOrigemPreco(cell.toString().trim());
+                        break;
+                    case 4:
+                        insumo.setPreco(Double.parseDouble(cell.toString()));
+                        break;
+                }
+
+                if(allFieldsFilled(insumo)) insumos.add(insumo);
+            }
+        }
+        return insumos;
+
+    }
+
+    private static boolean allFieldsFilled(Object object) {
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                if (field.get(object) == null) {
+                    return false;
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return true;
+    }
 }
