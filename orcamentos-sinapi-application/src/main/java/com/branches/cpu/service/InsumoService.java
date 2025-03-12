a23e2b5030e704bf265d27ddc10abbcae2fc5393
mbranches <marcusvbranches@gmail.com>
2025-03-12 09:44:42 -0300

    feat: adicionando service para consumir a api

--- orcamentos-sinapi-application/src/main/java/com/branches/cpu/service/InsumoService.java
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/service/InsumoService.java
@@ -1,4 +1,28 @@
 package com.branches.cpu.service;
 
+import com.branches.cpu.model.Insumo;
+import com.branches.cpu.model.ServicoAdicionado;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.List;
+
 public class InsumoService {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String url = "http://localhost:8090/v1/insumos";
+
+    public List<Insumo> findAll() {
+        Insumo[] arrayInsumo = restTemplate.getForObject(url, Insumo[].class);
+        return Arrays.asList(arrayInsumo);
+    }
+
+    public List<Insumo> findByName(String description) {
+        String urlGetByName = url + "?description=" + description;
+
+        Insumo[] arrayInsumo = restTemplate.getForObject(urlGetByName, Insumo[].class);
+        return Arrays.asList(arrayInsumo);
+    }
 }
