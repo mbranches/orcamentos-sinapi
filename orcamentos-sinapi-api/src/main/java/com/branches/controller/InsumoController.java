852ade38d30d30eed359c6e970dd41c8082d94ca
Branches <marcus.branches@icloud.com>
2025-03-14 15:05:41 -0300

    refactor: assinatura dos métodos alteradas, agora os métodos retornar ResponseEntity

--- orcamentos-sinapi-api/src/main/java/com/branches/controller/InsumoController.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/controller/InsumoController.java
@@ -4,6 +4,10 @@ import com.branches.model.Insumo;
 import com.branches.request.InsumoPostRequest;
 import com.branches.service.InsumoService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +19,13 @@ public class InsumoController {
     private final InsumoService SERVICE;
 
     @GetMapping
-    public List<Insumo> findAll(@RequestParam(required = false) String description) {
-        return description == null ? SERVICE.findAll() : SERVICE.findByDescription(description);
+    public ResponseEntity<List<Insumo>> findAll(@RequestParam(required = false) String description) {
+        return description == null ? ResponseEntity.ok(SERVICE.findAll()) : ResponseEntity.ok(SERVICE.findByDescription(description));
     }
 
     @PostMapping
-    public Insumo save(@RequestBody InsumoPostRequest insumoPostRequest){
-        return SERVICE.save(insumoPostRequest);
+    public ResponseEntity<Insumo> save(@RequestBody InsumoPostRequest insumoPostRequest){
+        Insumo response = SERVICE.save(insumoPostRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- orcamentos-sinapi-api/src/main/java/com/branches/controller/OrcamentoController.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/controller/OrcamentoController.java
@@ -4,6 +4,8 @@ import com.branches.model.Orcamento;
 import com.branches.request.OrcamentoPostRequest;
 import com.branches.service.OrcamentoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,8 @@ public class OrcamentoController {
     private final OrcamentoService SERVICE;
 
     @PostMapping
-    public Orcamento save(@RequestBody OrcamentoPostRequest orcamentoPostRequest) {
-        return SERVICE.save(orcamentoPostRequest);
+    public ResponseEntity<Orcamento> save(@RequestBody OrcamentoPostRequest orcamentoPostRequest) {
+        Orcamento response = SERVICE.save(orcamentoPostRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
