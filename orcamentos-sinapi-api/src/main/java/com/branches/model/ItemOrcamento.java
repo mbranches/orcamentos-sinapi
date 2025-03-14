b02984887e502cb2a1f33c7c571e42639d4e29e4
mbranches <marcusvbranches@gmail.com>
2025-03-14 14:35:00 -0300

    feat: teste unitario adicionado à ItemOrcamentoServiece

--- orcamentos-sinapi-api/src/main/java/com/branches/model/ItemOrcamento.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/model/ItemOrcamento.java
@@ -1,12 +1,14 @@
 package com.branches.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity(name = "item_orcamento")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemOrcamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
