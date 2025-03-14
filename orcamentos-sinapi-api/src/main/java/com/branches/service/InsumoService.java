c4b56f78ab625701f85ee624b2b340f45485bff0
mbranches <marcusvbranches@gmail.com>
2025-03-14 14:40:07 -0300

    refactor: variaveis finais postas em upper case

--- orcamentos-sinapi-api/src/main/java/com/branches/service/InsumoService.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/service/InsumoService.java
@@ -12,20 +12,20 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class InsumoService {
-    private final InsumoRepository repository;
+    private final InsumoRepository REPOSITORY;
     private final InsumoMapper MAPPER;
 
 
     public List<Insumo> findAll() {
-        return repository.findAll();
+        return REPOSITORY.findAll();
     }
 
     public List<Insumo> findByDescription(String description) {
-        return repository.findAllByDescricaoContaining(description);
+        return REPOSITORY.findAllByDescricaoContaining(description);
     }
 
     public Insumo save(InsumoPostRequest insumoPostRequest) {
         Insumo insumo = MAPPER.toInsumo(insumoPostRequest);
-        return repository.save(insumo);
+        return REPOSITORY.save(insumo);
     }
 }
