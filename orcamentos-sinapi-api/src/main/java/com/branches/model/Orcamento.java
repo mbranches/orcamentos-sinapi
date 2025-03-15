37cf974436cea9e8fc7066239517ed9e5a9d6f92
mbranches <marcusvbranches@gmail.com>
2025-03-14 23:48:46 -0300

    feat: coluna usuário adicionado a orçamento

--- orcamentos-sinapi-api/src/main/java/com/branches/model/Orcamento.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/model/Orcamento.java
@@ -15,8 +15,10 @@ public class Orcamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "data_criacao")
-    private LocalDate dataCriacao;
     @Column(length = 50, nullable = false)
     private String nome;
+    @Column(name = "nome_cliente", length = 50, nullable = false)
+    private String nomeCliente;
+    @Column(name = "data_criacao")
+    private LocalDate dataCriacao;
 }
