365c53081b716d1450d082e5869b41abc2b0d47b
mbranches <marcusvbranches@gmail.com>
2025-03-15 10:48:03 -0300

    feat: nomeCliente adicionado à classe post

--- orcamentos-sinapi-application/src/main/java/com/branches/cpu/request/OrcamentoPostRequest.java
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/request/OrcamentoPostRequest.java
@@ -1,6 +1,5 @@
 package com.branches.cpu.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,4 +9,5 @@ import lombok.Setter;
 @Getter
 public class OrcamentoPostRequest {
     private String nome;
+    private String nomeCliente;
 }
