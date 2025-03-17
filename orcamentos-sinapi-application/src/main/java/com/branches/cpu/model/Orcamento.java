06e3023abd207149093651658950f319d96e3350
mbranches <marcusvbranches@gmail.com>
2025-03-17 11:41:34 -0300

    feat: setter adicionado à classe orçamento

--- orcamentos-sinapi-application/src/main/java/com/branches/cpu/model/Orcamento.java
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/model/Orcamento.java
@@ -1,10 +1,12 @@
 package com.branches.cpu.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 public class Orcamento {
     private Long id;
     private String nome;
