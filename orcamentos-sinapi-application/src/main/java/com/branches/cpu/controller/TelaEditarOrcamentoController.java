a642361132f84ee3840c932dc101f6b8904b502a
mbranches <marcusvbranches@gmail.com>
2025-03-17 11:40:59 -0300

    feat: api consumida para atualizar orçamento na tela de editar orçamento

--- orcamentos-sinapi-application/src/main/java/com/branches/cpu/controller/TelaEditarOrcamentoController.java
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/controller/TelaEditarOrcamentoController.java
@@ -1,6 +1,7 @@
 package com.branches.cpu.controller;
 
 import com.branches.cpu.model.Orcamento;
+import com.branches.cpu.service.OrcamentoService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,6 +18,10 @@ public class TelaEditarOrcamentoController {
     @FXML
     private TextField tfNomeOrcamento;
 
+    private Orcamento orcamentoAEditar;
+
+    private final OrcamentoService ORCAMENTO_SERVICE = new OrcamentoService();
+
     @FXML
     void Fechar(ActionEvent event) {
 
@@ -24,6 +29,20 @@ public class TelaEditarOrcamentoController {
 
     @FXML
     void salvarFechar(ActionEvent event) {
+        orcamentoAEditar.setNome(tfNomeOrcamento.getText());
+        orcamentoAEditar.setNomeCliente(tfNomeCliente.getText());
 
+        ORCAMENTO_SERVICE.update(orcamentoAEditar);
+    }
+
+    public void setOrcamento(Orcamento orcamento) {
+        this.orcamentoAEditar = orcamento;
+
+        atualizarCampos();
+    }
+
+    private void atualizarCampos() {
+        tfNomeOrcamento.setText(orcamentoAEditar.getNome());
+        tfNomeCliente.setText(orcamentoAEditar.getNomeCliente());
     }
 }
