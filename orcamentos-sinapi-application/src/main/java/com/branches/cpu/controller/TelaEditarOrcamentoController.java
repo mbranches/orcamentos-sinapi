d10c268f316383e76257c0ae5edd30b0ba0eb2f6
mbranches <marcusvbranches@gmail.com>
2025-03-17 11:18:54 -0300

    feat: tela editar orçamento criada

---
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/controller/TelaEditarOrcamentoController.java
@@ -0,0 +1,29 @@
+package com.branches.cpu.controller;
+
+import com.branches.cpu.model.Orcamento;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import lombok.Setter;
+
+public class TelaEditarOrcamentoController {
+    @FXML
+    private Button btnSalvarFechar;
+
+    @FXML
+    private TextField tfNomeCliente;
+
+    @FXML
+    private TextField tfNomeOrcamento;
+
+    @FXML
+    void Fechar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarFechar(ActionEvent event) {
+
+    }
+}
