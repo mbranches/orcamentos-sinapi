e88c24caa90933cef732a41408f061df003e2730
mbranches <marcusvbranches@gmail.com>
2025-03-18 16:43:55 -0300

    fix: lazy adicionado para resolver erro de circular dependency

--- orcamentos-sinapi-api/src/main/java/com/branches/service/OrcamentoService.java
+++ orcamentos-sinapi-api/src/main/java/com/branches/service/OrcamentoService.java
@@ -4,7 +4,7 @@ import com.branches.mapper.OrcamentoMapper;
 import com.branches.model.Orcamento;
 import com.branches.repository.OrcamentoRepository;
 import com.branches.request.OrcamentoPostRequest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,39 +14,44 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class OrcamentoService {
-    private final OrcamentoMapper MAPPER;
-    private final OrcamentoRepository REPOSITORY;
-    private final ItemOrcamentoService ITEM_SERVICE;
+    private OrcamentoMapper mapper;
+    private OrcamentoRepository repository;
+    private ItemOrcamentoService itemService;
+
+    public OrcamentoService(OrcamentoMapper mapper, OrcamentoRepository repository, @Lazy ItemOrcamentoService itemService) {
+        this.mapper = mapper;
+        this.repository = repository;
+        this.itemService = itemService;
+    }
 
     public Orcamento save(OrcamentoPostRequest orcamentoPostRequest) {
-        Orcamento orcamento = MAPPER.toOrcamento(orcamentoPostRequest);
+        Orcamento orcamento = mapper.toOrcamento(orcamentoPostRequest);
         orcamento.setDataCriacao(LocalDate.now());
-        return REPOSITORY.save(orcamento);
+        return repository.save(orcamento);
     }
 
     public List<Orcamento> findAll() {
-        return REPOSITORY.findAll();
+        return repository.findAll();
     }
 
     public Orcamento findByIdOrElseThrowNotFoundException(Long id) {
-        return REPOSITORY.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Orcamento Not Found"));
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Orcamento Not Found"));
     }
 
     public void update(Orcamento orcamento) {
         findByIdOrElseThrowNotFoundException(orcamento.getId());
-        REPOSITORY.save(orcamento);
+        repository.save(orcamento);
     }
 
     @Transactional
     public void delete(Long orcamentoId) {
         findByIdOrElseThrowNotFoundException(orcamentoId);
-        ITEM_SERVICE.deleteByOrcamentoId(orcamentoId);
-        REPOSITORY.deleteById(orcamentoId);
+        itemService.deleteByOrcamentoId(orcamentoId);
+        repository.deleteById(orcamentoId);
     }
 
     public List<Orcamento> findAllByName(String nameOrcamento) {
-        return REPOSITORY.findAllByNomeContaining(nameOrcamento);
+        return repository.findAllByNomeContaining(nameOrcamento);
     }
 }
