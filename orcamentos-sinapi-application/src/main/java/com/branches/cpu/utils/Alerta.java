5b90279ab5c0638acc1c09f40be7ed1a5c0c0610
Branches <marcus.branches@icloud.com>
2025-03-18 18:09:07 -0300

    feat: alerta de erro adicionado

--- orcamentos-sinapi-application/src/main/java/com/branches/cpu/utils/Alerta.java
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/utils/Alerta.java
@@ -1,6 +1,7 @@
 package com.branches.cpu.utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
 public class Alerta {
@@ -9,14 +10,14 @@ public class Alerta {
     private static boolean resposta;
 
     public static void salvo(String objetoSalvo, String mensagem) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.INFORMATION);
         alert.setHeaderText(objetoSalvo);
         alert.setContentText(mensagem);
         alert.show();
     }
 
     public static boolean confirmarExclusão(String tipoExclusao, String msgExclusao) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setHeaderText("Exclusão de " + tipoExclusao);
         alert.setContentText("Excluir \"" + msgExclusao +"\" ?");
         alert.getButtonTypes().setAll(btnConfirmar, btnCancelar);
@@ -28,5 +29,12 @@ public class Alerta {
                         }
                 );
         return resposta;
+    }
+
+    public static void error(String mensagem) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setHeaderText("Error");
+        alert.setContentText(mensagem);
+        alert.show();
     }
 }
