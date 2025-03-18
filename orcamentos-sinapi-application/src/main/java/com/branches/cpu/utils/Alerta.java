0b15014b5f69df8a7c14e364f19eae95d5e99c6c
mbranches <marcusvbranches@gmail.com>
2025-03-18 17:10:51 -0300

    feat: alert para perguntar se o cliente deseja mesmo excluir orçamento

---
+++ orcamentos-sinapi-application/src/main/java/com/branches/cpu/utils/Alerta.java
@@ -0,0 +1,25 @@
+package com.branches.cpu.utils;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class Alerta {
+    private static ButtonType btnConfirmar = new ButtonType("Confirmar");
+    private static ButtonType btnCancelar = new ButtonType("Cancelar");
+    private static boolean resposta;
+
+    public static boolean confirmarExclusão(String tipoExclusao, String msgExclusao) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Exclusão de " + tipoExclusao);
+        alert.setContentText("Excluir \"" + msgExclusao +"\" ?");
+        alert.getButtonTypes().setAll(btnConfirmar, btnCancelar);
+        alert.showAndWait()
+                .ifPresent(
+                        btn -> {
+                            if (btn == btnConfirmar) resposta = true;
+                            else resposta = false;
+                        }
+                );
+        return resposta;
+    }
+}
