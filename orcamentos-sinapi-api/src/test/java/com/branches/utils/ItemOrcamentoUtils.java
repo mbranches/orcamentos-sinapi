9763d93f5717365ee0829b8fe64002a38e39cbb0
mbranches <marcusvbranches@gmail.com>
2025-04-24 14:58:35 -0300

    feat: cria classe util para itemOrcamento

---
+++ orcamentos-sinapi-api/src/test/java/com/branches/utils/ItemOrcamentoUtils.java
@@ -0,0 +1,62 @@
+package com.branches.utils;
+
+import com.branches.model.ItemOrcamento;
+import com.branches.model.Orcamento;
+import com.branches.request.ItemOrcamentoPostRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemOrcamentoUtils {
+    public static List<ItemOrcamento> newItemOrcamentoList() {
+        Orcamento orcamento = OrcamentoUtils.createsOrcamento();
+        ItemOrcamento item1 = ItemOrcamento.builder()
+                .id(1L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(1)
+                .orcamento(orcamento)
+                .build();
+
+        ItemOrcamento item2 = ItemOrcamento.builder()
+                .id(2L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(3)
+                .orcamento(orcamento)
+                .build();
+
+        ItemOrcamento item3 = ItemOrcamento.builder()
+                .id(3L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(89)
+                .orcamento(orcamento)
+                .build();
+
+        return new ArrayList<>(List.of(item1, item2, item3));
+    }
+
+    public static List<ItemOrcamentoPostRequest> newItemPostRequestList() {
+        Orcamento orcamento = OrcamentoUtils.createsOrcamento();
+        ItemOrcamentoPostRequest item1 = ItemOrcamentoPostRequest.builder()
+                .id(1L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(1)
+                .orcamento(orcamento)
+                .build();
+
+        ItemOrcamentoPostRequest item2 = ItemOrcamentoPostRequest.builder()
+                .id(2L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(3)
+                .orcamento(orcamento)
+                .build();
+
+        ItemOrcamentoPostRequest item3 = ItemOrcamentoPostRequest.builder()
+                .id(3L)
+                .insumo(InsumoUtils.createsInsumo())
+                .quantidade(89)
+                .orcamento(orcamento)
+                .build();
+
+        return new ArrayList<>(List.of(item1, item2, item3));
+    }
+}
