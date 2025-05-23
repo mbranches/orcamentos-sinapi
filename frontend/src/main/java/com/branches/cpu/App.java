a8b7bc4053a25070fb0810ff9f1e24e9c6efe6cc
mbranches <marcusvbranches@gmail.com>
2025-05-23 18:16:04 -0300

    feat: adiciona ícone da tela inicial

--- frontend/src/main/java/com/branches/cpu/App.java
+++ frontend/src/main/java/com/branches/cpu/App.java
@@ -3,13 +3,12 @@ package com.branches.cpu;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     @Override
@@ -18,12 +17,20 @@ public class App extends Application {
         
         Scene scene = new Scene(fxmlLoader.load());
 
-        stage.setTitle("Branches Orçamentos");
-        stage.setMinWidth(720);
-        stage.setMinHeight(480);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+        try(InputStream inputStream = getClass().getResourceAsStream("/com/branches/cpu/Images/application-icon.png")) {
+            Image icon = new Image(inputStream);
+
+            stage.setTitle("Branches Orçamentos");
+            stage.setMinWidth(720);
+            stage.setMinHeight(480);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.getIcons().setAll(icon);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Não foi possível carregar a tela inicial");
+            throw e;
+        }
     }
     
     public static void main(String[] args) {
